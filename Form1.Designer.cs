59aafcdc81357ff3d96a2d98857dd7fa2bbbc828
SznAltn <105235609+SznAltn@users.noreply.github.com>
2022-07-27 23:22:56 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace NKatmanliMimari1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtID = new System.Windows.Forms.TextBox();
+            this.BtnListele = new System.Windows.Forms.Button();
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtSoyad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtMaas = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtGorev = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtSehir = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.BtnEkle = new System.Windows.Forms.Button();
+            this.BtnSil = new System.Windows.Forms.Button();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 147);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(697, 208);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(44, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID:";
+            // 
+            // TxtID
+            // 
+            this.TxtID.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtID.Location = new System.Drawing.Point(86, 13);
+            this.TxtID.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtID.Name = "TxtID";
+            this.TxtID.Size = new System.Drawing.Size(148, 26);
+            this.TxtID.TabIndex = 2;
+            // 
+            // BtnListele
+            // 
+            this.BtnListele.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnListele.Location = new System.Drawing.Point(553, 9);
+            this.BtnListele.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnListele.Name = "BtnListele";
+            this.BtnListele.Size = new System.Drawing.Size(112, 25);
+            this.BtnListele.TabIndex = 3;
+            this.BtnListele.Text = "Listele";
+            this.BtnListele.UseVisualStyleBackColor = true;
+            this.BtnListele.Click += new System.EventHandler(this.BtnListele_Click);
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtAd.Location = new System.Drawing.Point(86, 57);
+            this.TxtAd.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(148, 26);
+            this.TxtAd.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(40, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "AD:";
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtSoyad.Location = new System.Drawing.Point(86, 104);
+            this.TxtSoyad.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(148, 26);
+            this.TxtSoyad.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(5, 104);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "SOYAD:";
+            // 
+            // TxtMaas
+            // 
+            this.TxtMaas.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtMaas.Location = new System.Drawing.Point(352, 96);
+            this.TxtMaas.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtMaas.Name = "TxtMaas";
+            this.TxtMaas.Size = new System.Drawing.Size(148, 26);
+            this.TxtMaas.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(281, 99);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "MAAŞ:";
+            // 
+            // TxtGorev
+            // 
+            this.TxtGorev.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtGorev.Location = new System.Drawing.Point(352, 16);
+            this.TxtGorev.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtGorev.Name = "TxtGorev";
+            this.TxtGorev.Size = new System.Drawing.Size(148, 26);
+            this.TxtGorev.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(269, 19);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 18);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "GÖREV:";
+            // 
+            // TxtSehir
+            // 
+            this.TxtSehir.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtSehir.Location = new System.Drawing.Point(352, 52);
+            this.TxtSehir.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtSehir.Name = "TxtSehir";
+            this.TxtSehir.Size = new System.Drawing.Size(148, 26);
+            this.TxtSehir.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(275, 55);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 18);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "ŞEHİR:";
+            // 
+            // BtnEkle
+            // 
+            this.BtnEkle.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnEkle.Location = new System.Drawing.Point(553, 42);
+            this.BtnEkle.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnEkle.Name = "BtnEkle";
+            this.BtnEkle.Size = new System.Drawing.Size(112, 23);
+            this.BtnEkle.TabIndex = 14;
+            this.BtnEkle.Text = "Ekle";
+            this.BtnEkle.UseVisualStyleBackColor = true;
+            this.BtnEkle.Click += new System.EventHandler(this.BtnEkle_Click);
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnSil.Location = new System.Drawing.Point(553, 73);
+            this.BtnSil.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(112, 24);
+            this.BtnSil.TabIndex = 15;
+            this.BtnSil.Text = "Sil";
+            this.BtnSil.UseVisualStyleBackColor = true;
+            this.BtnSil.Click += new System.EventHandler(this.BtnSil_Click);
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnGuncelle.Location = new System.Drawing.Point(553, 106);
+            this.BtnGuncelle.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(112, 24);
+            this.BtnGuncelle.TabIndex = 16;
+            this.BtnGuncelle.Text = "Güncelle";
+            this.BtnGuncelle.UseVisualStyleBackColor = true;
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 469);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.BtnSil);
+            this.Controls.Add(this.BtnEkle);
+            this.Controls.Add(this.TxtSehir);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtGorev);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtMaas);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnListele);
+            this.Controls.Add(this.TxtID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtID;
+        private System.Windows.Forms.Button BtnListele;
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtSoyad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtMaas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtGorev;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtSehir;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button BtnEkle;
+        private System.Windows.Forms.Button BtnSil;
+        private System.Windows.Forms.Button BtnGuncelle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EntityLayer;
+using DataAccessLayer;
+using LogicLayer;
+
+
+namespace NKatmanliMimari1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            List<EntityPersonel> PerList = LogicPersonel.LLPersonelListesi();
+            dataGridView1.DataSource = PerList;
+
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            EntityPersonel ent = new EntityPersonel();
+            ent.Ad = TxtAd.Text;
+            ent.Soyad = TxtSoyad.Text;
+            ent.Gorev = TxtGorev.Text;
+            ent.Sehir = TxtSehir.Text;
+            ent.Maas = short.Parse(TxtMaas.Text);
+            LogicPersonel.LLPersonelEkle(ent);
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            EntityPersonel ent = new EntityPersonel();
+            ent.Id = Convert.ToInt32(TxtID.Text);
+            LogicPersonel.LLPersonelSil(ent.Id);
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+
+            EntityPersonel ent = new EntityPersonel();
+            ent.Id = Convert.ToInt32(TxtID.Text);
+            ent.Ad = TxtAd.Text;
+            ent.Soyad = TxtSoyad.Text;
+            ent.Gorev = TxtGorev.Text;
+            ent.Sehir = TxtSehir.Text;
+            ent.Maas = short.Parse (TxtMaas.Text);
+            LogicPersonel.LLPersonelGuncelle(ent);
+
+
+        }
+    }
+}
